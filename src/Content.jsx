ae33cf492986c961cd7e17a80e81cce0f30c2a87
Makayla <makaylabusher@gmail.com>
2023-01-24 12:23:52 -0600

    add awesome button

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,11 @@
+import { AwesomeButton } from 'react-awesome-button';
+import 'react-awesome-button/dist/styles.css';
+
 export function Content() {
   return (
     <div>
       <h1>Welcome to React!</h1>
+      <AwesomeButton type="primary">Button</AwesomeButton>
     </div>
   );
 }
